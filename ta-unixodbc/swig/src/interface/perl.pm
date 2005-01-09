f246b3d2ce79dd64f733d3a449f2ebcd15815e26
konieczp <konieczp@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2005-01-09 20:46:28 +0000

    use strict

--- ta-unixodbc/swig/src/interface/perl.pm
+++ ta-unixodbc/swig/src/interface/perl.pm
@@ -1,3 +1,3 @@
 # Initialization of UnixODBC added at the end of the module
 #
-$INITIALIZED && (TA_UNIXODBC_Initialize() == $TA_SUCCESS);
+$INITIALIZED && (TA_UNIXODBC_Initialize() == $Finance::TA::TA_SUCCESS);
