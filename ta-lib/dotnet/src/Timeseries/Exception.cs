d6eaf0b32acf732fe0ac364112ffe34547b491e1
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2005-11-27 04:34:46 +0000

    First Version

---
+++ ta-lib/dotnet/src/Timeseries/Exception.cs
@@ -0,0 +1,18 @@
+/****************************** BIG WARNING ********************
+ *   Only the code in TA-Lib-Core for .NET is mature.
+ * 
+ *   All code in TA-Lib-Timeseries is in development and is 
+ *   not yet ready for use in applications.
+ ****************************** BIG WARNING ********************/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TA.Lib
+{
+    class InternalError : System.ApplicationException
+    {
+        public InternalError() : base("Internal Error"){}
+    }
+}
