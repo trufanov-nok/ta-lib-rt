58fdcaad44ed4e7464ab629adbad7f45f114033f
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2005-12-18 04:31:33 +0000

    Fix related to circular buffer.

--- ta-lib/dotnet/src/Tests/CoreTests.cs
+++ ta-lib/dotnet/src/Tests/CoreTests.cs
@@ -1,10 +1,3 @@
-/****************************** BIG WARNING ********************
- *   Only the code in TA-Lib-Core for .NET is mature.
- * 
- *   All code in TA-Lib-Timeseries is in development and is 
- *   not yet ready for use in applications.
- ****************************** BIG WARNING ********************/
-
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,6 +11,8 @@ namespace Tests
         {
             simpleMACall();
             averageTrueRangeTest();
+            callMFI();
+            callHT();
         }
 
         void simpleMACall()
@@ -50,6 +45,39 @@ namespace Tests
             int outBegIdx;
             int outNbElement;
             Core.ATR(0, 9, input, input, input, 4, out outBegIdx, out outNbElement, output);
+        }
+
+        void callMFI()
+        {
+            // Fill up some input with dummy data.
+            double[] input = new double[10];
+            int[] inputInt = new int[10];
+            double[] output = new double[10];
+            for (int i = 0; i < 10; i++)
+            {
+                input[i] = (double)i;
+            }
+
+            // Perform the call.
+            int outBegIdx;
+            int outNbElement;
+            Core.MFI(0, 9, input, input, input, inputInt, 4, out outBegIdx, out outNbElement, output);
+        }
+
+        void callHT()
+        {
+            // Fill up some input with dummy data.
+            double[] input = new double[200];
+            double[] output = new double[200];
+            for (int i = 0; i < 200; i++)
+            {
+                input[i] = (double)i;
+            }
+
+            // Perform the call.
+            int outBegIdx;
+            int outNbElement;
+            Core.HT_DCPERIOD(0, 199, input, out outBegIdx, out outNbElement, output);
         }
 
     }
