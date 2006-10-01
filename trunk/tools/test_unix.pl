3a5c3c70e34df5700202d774500402aa5efa5c61
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2006-10-01 06:55:41 +0000

    Make the script more friendly to Unix users, Win32 users and my particular VMWare setup.

--- trunk/tools/test_unix.pl
+++ trunk/tools/test_unix.pl
@@ -1,24 +1,12 @@
 #!/usr/bin/perl
 
-# A lot more of initial clean-up is done by
-# the script run on the "windows" side (see release.pl).
-#
-# Before an offical release, make sure release.pl was
-# run with success first.
-# 
-
 use Cwd;
 use myUtil;
 
 sub Main
 {
-   $source_dir  = "/mnt/hgfs/Shared/";
-   $release_dir = "/mnt/hgfs/Release/";
-   $user_dir    = "/home/buzzo/";
-   $top_dir     = $user_dir."temp/";
-
-   open(STDOUT, ">".$release_dir."log/stdout_unix.txt" ) or die;
-   open(STDERR, ">".$release_dir."log/stderr_unix.txt" ) or die;
+   $workdir = &getWorkdir();
+   $tempdir = $workdir.'temp/unix/';
 
    if( $ARGV[0] eq "-fast" )
    {
@@ -30,33 +18,51 @@ sub Main
       $fastOption = 0;
    }
 
-   # Clean-up release directory
-   # removeRelease( '/mnt/hgfs/Shared/');   
-
-   # Clean-up possible temp files
-   # removeAllTempFile( '/mnt/hgfs/Shared/ta-lib/' );
-   # removeAllBinFile( '/mnt/hgfs/Shared/ta-lib/' );
+   open(OLD_STDERR,">&STDERR") or die "Failed to save STDERR";
+   open(OLD_STDOUT,">&STDOUT") or die "Failed to save STDOUT";   
+   open(STDOUT, ">".$workdir."log/stdout_unix.txt" ) or die;
+   open(STDERR, ">".$workdir."log/stderr_unix.txt" ) or die;
+   
+   # Clean-up working directory
+   createWorkdir();
 
    # Make a copy of ta-lib into the user directory.
    if( $fastOption eq 0 )
-   {
-     execProg( $user_dir, 'rm -r -f '.$top_dir );
-     execProg( $user_dir, 'mkdir '.$top_dir );
-     print( "\nStarting copy..." );
-     execProg( $top_dir,  "cp -r ".$source_dir."ta-lib ." );
-     print( "done.\n" );
+   {     
+     # If the directory "temp/win32/sourcecopy" exists in the workdir, 
+     # use it instead of making a copy from the source tree.
+     # 
+     # This is a speed optimization on my setup when I do test on unix 
+     # following successful tests on win32.
+     if( -e $workdir.'temp/win32/sourcecopy' )
+     {
+        $tempdir = $workdir.'temp/win32/sourcecopy';
+     }
+     else
+     { 
+       system( 'cp -r ../ta-lib '.$tempdir );
+     }
+    
+     # Clean-up possible temp files
+     removeAllTempFile( $tempdir );
+     removeAllBinFile( $tempdir );
    }
 
-   testGCC( $top_dir."ta-lib/", "cmd", $fastOption, 0, $release_dir."log/", 0 );
-   testGCC( $top_dir."ta-lib/", "csr", $fastOption, 0, $release_dir."log/", 0 );
+   testGCC( $tempdir.'ta-lib/', "cmd", $fastOption, 0, $workdir."log/", 0 );
+   testGCC( $tempdir.'ta-lib/', "csr", $fastOption, 0, $workdir."log/", 0 );
 
    if( $fastOption eq 0 )
    {
-      testGCC( $top_dir."ta-lib/", "cmr", $fastOption, 0, $release_dir."log/", 0 );
-      testGCC( $top_dir."ta-lib/", "csd", $fastOption, 0, $release_dir."log/", 0 );
+      testGCC( $tempdir.'ta-lib/', "cmr", $fastOption, 0, $workdir."log/", 0 );
+      testGCC( $tempdir.'ta-lib/', "csd", $fastOption, 0, $workdir."log/", 0 );
    }
 
    print "\n*** Success - All Unix Tests Completed ***\n";
+   #close STDOUT;
+   #close STDERR;
+   #open(STDOUT,">&OLD_STDOUT") or warn "Failed to restore STDOUT";
+   #open(STDERR,">&OLD_STDERR") or warn "Failed to restore STDERR";
+   #print "\n*** Success - All Unix Tests Completed ***\n";
 }
 
 &Main;				 
