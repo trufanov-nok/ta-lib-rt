aea0f60e9a6ef7794409d2edd48c1adc7979bd31
fortier <fortier@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2007-01-10 07:55:55 +0000

    A few update related to ta_BETA

--- ta-lib/java/src/com/tictactec/ta/lib/Core.java
+++ ta-lib/java/src/com/tictactec/ta/lib/Core.java
@@ -1729,141 +1729,6 @@ public class Core {
       return RetCode.Success ;
    }
    /* Generated */
-   public int betaLookback( )
-   {
-      return 1;
-   }
-   public RetCode beta( int startIdx,
-      int endIdx,
-      double inReal0[],
-      double inReal1[],
-      MInteger outBegIdx,
-      MInteger outNbElement,
-      double outReal[] )
-   {
-      double S_xx = 0.0f;
-      double S_yy = 0.0f;
-      double S_xy = 0.0f;
-      double S_x = 0.0f;
-      double S_y = 0.0f;
-      double last_price_x = 0.0f;
-      double last_price_y = 0.0f;
-      double tmp_real = 0.0f;
-      double x;
-      double y;
-      double n = 0.0f;
-      int i, outIdx;
-      if( startIdx < 0 )
-         return RetCode.OutOfRangeStartIndex ;
-      if( (endIdx < 0) || (endIdx < startIdx))
-         return RetCode.OutOfRangeEndIndex ;
-      if( startIdx < 1 )
-         startIdx = 1;
-      if( startIdx > endIdx )
-      {
-         outBegIdx.value = 0 ;
-         outNbElement.value = 0 ;
-         return RetCode.Success ;
-      }
-      outBegIdx.value = startIdx;
-      outIdx = 0;
-      last_price_x = inReal0[startIdx-1];
-      last_price_y = inReal1[startIdx-1];
-      for( i = startIdx; i <= endIdx; i++ )
-      {
-         tmp_real = inReal0[i];
-         if( ! (((-0.00000001)<last_price_x)&&(last_price_x<0.00000001)) )
-            x = (tmp_real-last_price_x)/last_price_x;
-         else
-            x = 0.0;
-         last_price_x = tmp_real;
-         tmp_real = inReal1[i];
-         if( ! (((-0.00000001)<last_price_y)&&(last_price_y<0.00000001)) )
-            y = (tmp_real-last_price_y)/last_price_y;
-         else
-            y = 0.0;
-         last_price_y = tmp_real;
-         S_xx += x*x;
-         S_yy += y*y;
-         S_xy += x*y;
-         S_x += x;
-         S_y += y;
-         n += 1.0f;
-         tmp_real = (n * S_xx) - (S_x * S_x);
-         if( ! (((-0.00000001)<tmp_real)&&(tmp_real<0.00000001)) )
-            outReal[outIdx++] = ((n * S_xy) - (S_x * S_y)) / tmp_real;
-         else
-            outReal[outIdx++] = 0.0;
-      }
-      outNbElement.value = outIdx;
-      return RetCode.Success ;
-   }
-   public RetCode beta( int startIdx,
-      int endIdx,
-      float inReal0[],
-      float inReal1[],
-      MInteger outBegIdx,
-      MInteger outNbElement,
-      double outReal[] )
-   {
-      double S_xx = 0.0f;
-      double S_yy = 0.0f;
-      double S_xy = 0.0f;
-      double S_x = 0.0f;
-      double S_y = 0.0f;
-      double last_price_x = 0.0f;
-      double last_price_y = 0.0f;
-      double tmp_real = 0.0f;
-      double x;
-      double y;
-      double n = 0.0f;
-      int i, outIdx;
-      if( startIdx < 0 )
-         return RetCode.OutOfRangeStartIndex ;
-      if( (endIdx < 0) || (endIdx < startIdx))
-         return RetCode.OutOfRangeEndIndex ;
-      if( startIdx < 1 )
-         startIdx = 1;
-      if( startIdx > endIdx )
-      {
-         outBegIdx.value = 0 ;
-         outNbElement.value = 0 ;
-         return RetCode.Success ;
-      }
-      outBegIdx.value = startIdx;
-      outIdx = 0;
-      last_price_x = inReal0[startIdx-1];
-      last_price_y = inReal1[startIdx-1];
-      for( i = startIdx; i <= endIdx; i++ )
-      {
-         tmp_real = inReal0[i];
-         if( ! (((-0.00000001)<last_price_x)&&(last_price_x<0.00000001)) )
-            x = (tmp_real-last_price_x)/last_price_x;
-         else
-            x = 0.0;
-         last_price_x = tmp_real;
-         tmp_real = inReal1[i];
-         if( ! (((-0.00000001)<last_price_y)&&(last_price_y<0.00000001)) )
-            y = (tmp_real-last_price_y)/last_price_y;
-         else
-            y = 0.0;
-         last_price_y = tmp_real;
-         S_xx += x*x;
-         S_yy += y*y;
-         S_xy += x*y;
-         S_x += x;
-         S_y += y;
-         n += 1.0f;
-         tmp_real = (n * S_xx) - (S_x * S_x);
-         if( ! (((-0.00000001)<tmp_real)&&(tmp_real<0.00000001)) )
-            outReal[outIdx++] = ((n * S_xy) - (S_x * S_y)) / tmp_real;
-         else
-            outReal[outIdx++] = 0.0;
-      }
-      outNbElement.value = outIdx;
-      return RetCode.Success ;
-   }
-   /* Generated */
    public int bbandsLookback( int optInTimePeriod,
       double optInNbDevUp,
       double optInNbDevDn,
@@ -2137,6 +2002,243 @@ public class Core {
             outRealLowerBand[i] = tempReal2 - (tempReal * optInNbDevDn);
          }
       }
+      return RetCode.Success ;
+   }
+   /* Generated */
+   public int betaLookback( int optInTimePeriod )
+   {
+      if( (int)optInTimePeriod == ( Integer.MIN_VALUE ) )
+         optInTimePeriod = 5;
+      else if( ((int)optInTimePeriod < 1) || ((int)optInTimePeriod > 100000) )
+         return -1;
+      return optInTimePeriod - 1;
+   }
+   public RetCode beta( int startIdx,
+      int endIdx,
+      double inReal0[],
+      double inReal1[],
+      int optInTimePeriod,
+      MInteger outBegIdx,
+      MInteger outNbElement,
+      double outReal[] )
+   {
+      double S_xx = 0.0f;
+      double S_yy = 0.0f;
+      double S_xy = 0.0f;
+      double S_x = 0.0f;
+      double S_y = 0.0f;
+      double last_price_x = 0.0f;
+      double last_price_y = 0.0f;
+      double trailing_last_price_x = 0.0f;
+      double trailing_last_price_y = 0.0f;
+      double tmp_real = 0.0f;
+      double x;
+      double y;
+      int i, outIdx;
+      int trailingIdx, nbInitialElementNeeded;
+      if( startIdx < 0 )
+         return RetCode.OutOfRangeStartIndex ;
+      if( (endIdx < 0) || (endIdx < startIdx))
+         return RetCode.OutOfRangeEndIndex ;
+      if( (int)optInTimePeriod == ( Integer.MIN_VALUE ) )
+         optInTimePeriod = 5;
+      else if( ((int)optInTimePeriod < 1) || ((int)optInTimePeriod > 100000) )
+         return RetCode.BadParam ;
+      nbInitialElementNeeded = optInTimePeriod-1;
+      if( startIdx < nbInitialElementNeeded )
+         startIdx = nbInitialElementNeeded;
+      if( startIdx > endIdx )
+      {
+         outBegIdx.value = 0 ;
+         outNbElement.value = 0 ;
+         return RetCode.Success ;
+      }
+      trailingIdx = startIdx-nbInitialElementNeeded;
+      last_price_x = inReal0[trailingIdx];
+      last_price_y = inReal1[trailingIdx];
+      trailingIdx++;
+      trailing_last_price_x = inReal0[trailingIdx];
+      trailing_last_price_y = inReal1[trailingIdx];
+      i = trailingIdx;
+      while( i < startIdx )
+      {
+         tmp_real = inReal0[i];
+         if( ! (((-0.00000001)<last_price_x)&&(last_price_x<0.00000001)) )
+            x = (tmp_real-last_price_x)/last_price_x;
+         else
+            x = 0.0;
+         last_price_x = tmp_real;
+         tmp_real = inReal1[i++];
+         if( ! (((-0.00000001)<last_price_y)&&(last_price_y<0.00000001)) )
+            y = (tmp_real-last_price_y)/last_price_y;
+         else
+            y = 0.0;
+         last_price_y = tmp_real;
+         S_xx += x*x;
+         S_yy += y*y;
+         S_xy += x*y;
+         S_x += x;
+         S_y += y;
+      }
+      outIdx = 0;
+      do
+      {
+         tmp_real = inReal0[i];
+         if( ! (((-0.00000001)<last_price_x)&&(last_price_x<0.00000001)) )
+            x = (tmp_real-last_price_x)/last_price_x;
+         else
+            x = 0.0;
+         last_price_x = tmp_real;
+         tmp_real = inReal1[i++];
+         if( ! (((-0.00000001)<last_price_y)&&(last_price_y<0.00000001)) )
+            y = (tmp_real-last_price_y)/last_price_y;
+         else
+            y = 0.0;
+         last_price_y = tmp_real;
+         S_xx += x*x;
+         S_yy += y*y;
+         S_xy += x*y;
+         S_x += x;
+         S_y += y;
+         tmp_real = (nbInitialElementNeeded * S_xx) - (S_x * S_x);
+         if( ! (((-0.00000001)<tmp_real)&&(tmp_real<0.00000001)) )
+            outReal[outIdx++] = ((nbInitialElementNeeded * S_xy) - (S_x * S_y)) / tmp_real;
+         else
+            outReal[outIdx++] = 0.0;
+         tmp_real = inReal0[trailingIdx];
+         if( ! (((-0.00000001)<trailing_last_price_x)&&(trailing_last_price_x<0.00000001)) )
+            x = (tmp_real-trailing_last_price_x)/trailing_last_price_x;
+         else
+            x = 0.0;
+         trailing_last_price_x = tmp_real;
+         tmp_real = inReal1[trailingIdx++];
+         if( ! (((-0.00000001)<trailing_last_price_y)&&(trailing_last_price_y<0.00000001)) )
+            y = (tmp_real-trailing_last_price_y)/trailing_last_price_y;
+         else
+            y = 0.0;
+         trailing_last_price_y = tmp_real;
+         S_xx -= x*x;
+         S_yy -= y*y;
+         S_xy -= x*y;
+         S_x -= x;
+         S_y -= y;
+      } while( i <= endIdx );
+      outNbElement.value = outIdx;
+      outBegIdx.value = startIdx;
+      return RetCode.Success ;
+   }
+   public RetCode beta( int startIdx,
+      int endIdx,
+      float inReal0[],
+      float inReal1[],
+      int optInTimePeriod,
+      MInteger outBegIdx,
+      MInteger outNbElement,
+      double outReal[] )
+   {
+      double S_xx = 0.0f;
+      double S_yy = 0.0f;
+      double S_xy = 0.0f;
+      double S_x = 0.0f;
+      double S_y = 0.0f;
+      double last_price_x = 0.0f;
+      double last_price_y = 0.0f;
+      double trailing_last_price_x = 0.0f;
+      double trailing_last_price_y = 0.0f;
+      double tmp_real = 0.0f;
+      double x;
+      double y;
+      int i, outIdx;
+      int trailingIdx, nbInitialElementNeeded;
+      if( startIdx < 0 )
+         return RetCode.OutOfRangeStartIndex ;
+      if( (endIdx < 0) || (endIdx < startIdx))
+         return RetCode.OutOfRangeEndIndex ;
+      if( (int)optInTimePeriod == ( Integer.MIN_VALUE ) )
+         optInTimePeriod = 5;
+      else if( ((int)optInTimePeriod < 1) || ((int)optInTimePeriod > 100000) )
+         return RetCode.BadParam ;
+      nbInitialElementNeeded = optInTimePeriod-1;
+      if( startIdx < nbInitialElementNeeded )
+         startIdx = nbInitialElementNeeded;
+      if( startIdx > endIdx )
+      {
+         outBegIdx.value = 0 ;
+         outNbElement.value = 0 ;
+         return RetCode.Success ;
+      }
+      trailingIdx = startIdx-nbInitialElementNeeded;
+      last_price_x = inReal0[trailingIdx];
+      last_price_y = inReal1[trailingIdx];
+      trailingIdx++;
+      trailing_last_price_x = inReal0[trailingIdx];
+      trailing_last_price_y = inReal1[trailingIdx];
+      i = trailingIdx;
+      while( i < startIdx )
+      {
+         tmp_real = inReal0[i];
+         if( ! (((-0.00000001)<last_price_x)&&(last_price_x<0.00000001)) )
+            x = (tmp_real-last_price_x)/last_price_x;
+         else
+            x = 0.0;
+         last_price_x = tmp_real;
+         tmp_real = inReal1[i++];
+         if( ! (((-0.00000001)<last_price_y)&&(last_price_y<0.00000001)) )
+            y = (tmp_real-last_price_y)/last_price_y;
+         else
+            y = 0.0;
+         last_price_y = tmp_real;
+         S_xx += x*x;
+         S_yy += y*y;
+         S_xy += x*y;
+         S_x += x;
+         S_y += y;
+      }
+      outIdx = 0;
+      do
+      {
+         tmp_real = inReal0[i];
+         if( ! (((-0.00000001)<last_price_x)&&(last_price_x<0.00000001)) )
+            x = (tmp_real-last_price_x)/last_price_x;
+         else
+            x = 0.0;
+         last_price_x = tmp_real;
+         tmp_real = inReal1[i++];
+         if( ! (((-0.00000001)<last_price_y)&&(last_price_y<0.00000001)) )
+            y = (tmp_real-last_price_y)/last_price_y;
+         else
+            y = 0.0;
+         last_price_y = tmp_real;
+         S_xx += x*x;
+         S_yy += y*y;
+         S_xy += x*y;
+         S_x += x;
+         S_y += y;
+         tmp_real = (nbInitialElementNeeded * S_xx) - (S_x * S_x);
+         if( ! (((-0.00000001)<tmp_real)&&(tmp_real<0.00000001)) )
+            outReal[outIdx++] = ((nbInitialElementNeeded * S_xy) - (S_x * S_y)) / tmp_real;
+         else
+            outReal[outIdx++] = 0.0;
+         tmp_real = inReal0[trailingIdx];
+         if( ! (((-0.00000001)<trailing_last_price_x)&&(trailing_last_price_x<0.00000001)) )
+            x = (tmp_real-trailing_last_price_x)/trailing_last_price_x;
+         else
+            x = 0.0;
+         trailing_last_price_x = tmp_real;
+         tmp_real = inReal1[trailingIdx++];
+         if( ! (((-0.00000001)<trailing_last_price_y)&&(trailing_last_price_y<0.00000001)) )
+            y = (tmp_real-trailing_last_price_y)/trailing_last_price_y;
+         else
+            y = 0.0;
+         trailing_last_price_y = tmp_real;
+         S_xx -= x*x;
+         S_yy -= y*y;
+         S_xy -= x*y;
+         S_x -= x;
+         S_y -= y;
+      } while( i <= endIdx );
+      outNbElement.value = outIdx;
+      outBegIdx.value = startIdx;
       return RetCode.Success ;
    }
    /* Generated */
