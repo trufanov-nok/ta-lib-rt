8c818152627e194f4bb34919015639da5c52ebaa
fgomes <fgomes@159cb52c-178a-4f3c-8eb8-d0aff033d058>
2007-03-05 23:09:40 +0000

    1. Fixing runt time information in FuncInfo.java 2. Adding a SimipleHelper class for late binding calls 3. Some tests cases added in TestCoreMetaData.java
    NOTE: The field 'hint' in FuncInfo.java still requires gen_code.c dumps its value properly.

--- trunk/ta-lib/java/src/com/tictactec/ta/lib/test/TestCoreMetaData.java
+++ trunk/ta-lib/java/src/com/tictactec/ta/lib/test/TestCoreMetaData.java
@@ -1,8 +1,12 @@
 package com.tictactec.ta.lib.test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
+import com.tictactec.ta.lib.MInteger;
 import com.tictactec.ta.lib.meta.CoreMetaData;
+import com.tictactec.ta.lib.meta.PriceHolder;
 import com.tictactec.ta.lib.meta.TaFuncService;
 import com.tictactec.ta.lib.meta.TaGrpService;
 import com.tictactec.ta.lib.meta.annotation.InputParameterInfo;
@@ -12,6 +16,7 @@ import com.tictactec.ta.lib.meta.annotation.OptInputParameterInfo;
 import com.tictactec.ta.lib.meta.annotation.OutputParameterInfo;
 import com.tictactec.ta.lib.meta.annotation.RealList;
 import com.tictactec.ta.lib.meta.annotation.RealRange;
+import com.tictactec.ta.lib.meta.helpers.SimpleHelper;
 
 public class TestCoreMetaData {
 
@@ -105,6 +110,168 @@ public class TestCoreMetaData {
             e.printStackTrace();
         }
         System.out.println("=====================================");
+
+    
+    
+        
+        
+        
+        SimpleHelper calc;
+
+        // input data
+        double open[] = {
+                1.4054, 1.4060, 1.4062, 1.4059, 1.4057, 1.4057, 1.4051, 1.4054, 1.4056, 1.4056,
+                1.4054, 1.4060, 1.4062, 1.4059, 1.4057, 1.4057, 1.4051, 1.4054, 1.4056, 1.4056,
+                1.4054, 1.4060, 1.4062, 1.4059, 1.4057, 1.4057, 1.4051, 1.4054, 1.4056, 1.4056,
+                1.4054, 1.4060, 1.4062, 1.4059, 1.4057, 1.4057, 1.4051, 1.4054, 1.4056, 1.4056,
+                1.4054, 1.4060, 1.4062, 1.4059, 1.4057, 1.4057, 1.4051, 1.4054, 1.4056, 1.4056,
+                1.4054, 1.4060, 1.4062, 1.4059, 1.4057, 1.4057, 1.4051, 1.4054, 1.4056, 1.4056
+        };
+
+        double high[] = {
+                1.4654, 1.4660, 1.4662, 1.4659, 1.4657, 1.4657, 1.4651, 1.4654, 1.4656, 1.4656,
+                1.4654, 1.4660, 1.4662, 1.4659, 1.4657, 1.4657, 1.4651, 1.4654, 1.4656, 1.4656,
+                1.4654, 1.4660, 1.4662, 1.4659, 1.4657, 1.4657, 1.4651, 1.4654, 1.4656, 1.4656,
+                1.4654, 1.4660, 1.4662, 1.4659, 1.4657, 1.4657, 1.4651, 1.4654, 1.4656, 1.4656,
+                1.4654, 1.4660, 1.4662, 1.4659, 1.4657, 1.4657, 1.4651, 1.4654, 1.4656, 1.4656,
+                1.4654, 1.4660, 1.4662, 1.4659, 1.4657, 1.4657, 1.4651, 1.4654, 1.4656, 1.4656
+        };
+
+
+        double low[] = {
+                1.3954, 1.3960, 1.3962, 1.3959, 1.3957, 1.3957, 1.3951, 1.3954, 1.3956, 1.3956,
+                1.3954, 1.3960, 1.3962, 1.3959, 1.3957, 1.3957, 1.3951, 1.3954, 1.3956, 1.3956,
+                1.3954, 1.3960, 1.3962, 1.3959, 1.3957, 1.3957, 1.3951, 1.3954, 1.3956, 1.3956,
+                1.3954, 1.3960, 1.3962, 1.3959, 1.3957, 1.3957, 1.3951, 1.3954, 1.3956, 1.3956,
+                1.3954, 1.3960, 1.3962, 1.3959, 1.3957, 1.3957, 1.3951, 1.3954, 1.3956, 1.3956,
+                1.3954, 1.3960, 1.3962, 1.3959, 1.3957, 1.3957, 1.3951, 1.3954, 1.3956, 1.3956
+        };
+
+        
+        double close[] = {
+                1.4554, 1.4560, 1.4562, 1.4559, 1.4557, 1.4557, 1.4551, 1.4554, 1.4556, 1.4556,
+                1.4554, 1.4560, 1.4562, 1.4559, 1.4557, 1.4557, 1.4551, 1.4554, 1.4556, 1.4556,
+                1.4554, 1.4560, 1.4562, 1.4559, 1.4557, 1.4557, 1.4551, 1.4554, 1.4556, 1.4556,
+                1.4554, 1.4560, 1.4562, 1.4559, 1.4557, 1.4557, 1.4551, 1.4554, 1.4556, 1.4556,
+                1.4554, 1.4560, 1.4562, 1.4559, 1.4557, 1.4557, 1.4551, 1.4554, 1.4556, 1.4556,
+                1.4554, 1.4560, 1.4562, 1.4559, 1.4557, 1.4557, 1.4551, 1.4554, 1.4556, 1.4556
+        };
+        // output buffers and return values
+        double output1[] = new double[60];
+        double output2[] = new double[60];
+        double output3[] = new double[60];
+        MInteger lOutIdx  = new MInteger();
+        MInteger lOutSize = new MInteger();
+
+        // function name and parameter holder
+        String func;
+        List<String> params = new ArrayList<String>();
+        
+        try {
+            /*
+                MAMA
+                    [TA_Input_Real]    [0] inReal
+                    [TA_OptInput_RealRange]   [0] optInFastLimit : 0.01..0.99 [0.01]
+                    [TA_OptInput_RealRange]   [0] optInSlowLimit : 0.01..0.99 [0.01]
+                    [TA_Output_Real]    [1] outMAMA
+                    [TA_Output_Real]    [4] outFAMA
+            */
+            func = "MAMA";
+            params.clear();
+            params.add("0.2");
+            params.add("0.02");
+            calc = new SimpleHelper(func, params);
+    
+            System.out.println("===============================================");
+            System.out.println(func);
+            calc.calculate(0, 59, new Object[] { close }, new Object[] { output1, output2 }, lOutIdx, lOutSize);
+            
+            System.out.println("lookback="+calc.getLookback());
+            System.out.println("outBegIdx    = "+lOutIdx.value+ "    outNbElement = "+lOutSize.value);
+            for (int i=0; i<lOutSize.value; i++) {
+                System.out.println("output1["+i+"]="+output1[i]+"     "+"output2["+i+"]="+output2[i]);
+            }
+
+            /*
+              BBANDS
+                [TA_Input_Real]    [0] inReal
+                [TA_OptInput_IntegerRange] [0] optInTimePeriod : 2..100000
+                [TA_OptInput_RealRange]   [0] optInNbDevUp : -3e+37..3e+37 [0.01]
+                [TA_OptInput_RealRange]   [0] optInNbDevDn : -3e+37..3e+37 [0.01]
+                [TA_OptInput_IntegerList] [0] optInMAType : SMA=0, EMA=1, WMA=2, DEMA=3, TEMA=4, TRIMA=5, KAMA=6, MAMA=7, T3=8
+                [TA_Output_Real]    [1] outRealUpperBand
+                [TA_Output_Real]    [1] outRealMiddleBand
+                [TA_Output_Real]    [1] outRealLowerBand
+             */
+            func = "bbands";
+            params.clear();
+            params.add("8");
+            params.add("0.02");
+            params.add("0.04");
+            params.add("dEmA");
+            
+            calc = new SimpleHelper(func, params);
+            
+            System.out.println("===============================================");
+            System.out.println(func);
+            calc.calculate(0, 59, new Object[] { close }, new Object[] { output1, output2, output3 }, lOutIdx, lOutSize);
+            
+            System.out.println("lookback="+calc.getLookback());
+            System.out.println("outBegIdx    = "+lOutIdx.value+ "    outNbElement = "+lOutSize.value);
+            for (int i=0; i<lOutSize.value; i++) {
+                System.out.println("output1["+i+"]="+output1[i]+"     "+"output2["+i+"]="+output2[i]+"     "+"output3["+i+"]="+output3[i]);
+            }
+            
+            /*
+                ADX
+                    [TA_Input_Price]   [14] inPriceHLC
+                    [TA_OptInput_IntegerRange] [0] optInTimePeriod : 2..100000
+                    [TA_Output_Real]    [1] outReal             
+            */
+            func = "Adx";
+            params.clear();
+            params.add("8");
+            
+            calc = new SimpleHelper(func, params);
+            
+            System.out.println("===============================================");
+            System.out.println(func);
+            int flags = calc.getMetaData().getInputParameterInfo(0).flags();
+            PriceHolder price = new PriceHolder(flags, open, high, low, close, null, null);
+            calc.calculate(0, 59, new Object[] { price }, new Object[] { output1 }, lOutIdx, lOutSize);
+            
+            System.out.println("lookback="+calc.getLookback());
+            System.out.println("outBegIdx    = "+lOutIdx.value+ "    outNbElement = "+lOutSize.value);
+            for (int i=0; i<lOutSize.value; i++) {
+                System.out.println("output1["+i+"]="+output1[i]);
+            }
+            
+            /*
+              RSI
+                [TA_Input_Real]    [0] inReal
+                [TA_OptInput_IntegerRange] [0] optInTimePeriod : 2..100000
+                [TA_Output_Real]    [1] outReal
+             */
+            func = "rsi";
+            params.clear();
+            params.add("8");
+            
+            calc = new SimpleHelper(func, params);
+            
+            System.out.println("===============================================");
+            System.out.println(func);
+            calc.calculate(0, 59, new Object[] { close }, new Object[] { output1 }, lOutIdx, lOutSize);
+            
+            System.out.println("lookback="+calc.getLookback());
+            System.out.println("outBegIdx    = "+lOutIdx.value+ "    outNbElement = "+lOutSize.value);
+            for (int i=0; i<lOutSize.value; i++) {
+                System.out.println("output1["+i+"]="+output1[i]);
+            }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    
     }
 
 }
