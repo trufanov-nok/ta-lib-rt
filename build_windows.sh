a0c40a5535c3191208a168568fd5aca38254b204
Brian Petkovsek <16124109+BrianPetkovsek@users.noreply.github.com>
2023-04-22 16:29:43 -0400

    push

--- build_windows.sh
+++ build_windows.sh
@@ -2,5 +2,5 @@
 sudo apt-get install mingw-w64
 cd ta-lib-rt
 mkdir build && cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=./mingw-w64-x86_64.cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=mingw-w64-x86_64.cmake ..
 make
